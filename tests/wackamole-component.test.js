583539fea58e2c17f5eafc1b6f338d09141a960c
Rafael Minguet <rminguet@gmail.com>
2017-04-18 12:15:17 +0200

    Day 18-04-2017

--- tests/wackamole-component.test.js
+++ tests/wackamole-component.test.js
@@ -3,35 +3,45 @@ import test from 'tape';
 // Component to test
 
 const WackAMole = function () {
-    let mole = false;
     let score = 0;
 
-    const popMole = function () {
-        mole = true;
+    const holes = [false, false, false, false, false, false];
+
+    const popMole = function (hole = 0) {
+        if (holes[hole]) {
+            return false;
+        }
+        holes[hole] = true;
+        return true;
     };
 
-    const hideMole = function () {
-        mole = false;
+    const hideMole = function (hole = 0) {
+        if (!holes[hole]) {
+            return false;
+        }
+        holes[hole] = false;
+        return true;
     };
 
-    const isMoleVisible = function () {
-        return mole;
+    const isMoleVisible = function (hole = 0) {
+        return holes[hole];
     };
 
     const addPoint = function () {
         score++;
     };
 
-    const hit = function () {
-        hideMole();
-        addPoint();
+    const hit = function (hole = 0) {
+        if (hideMole(hole)) {
+            addPoint();
+        }
     };
 
     const getScore = function () {
         return score;
     };
 
-    return {popMole, isMoleVisible, hideMole, addPoint, getScore, hit};
+    return {popMole, hideMole, isMoleVisible, addPoint, getScore, holes, hit};
 };
 
 // Testear que cuando creamos una instancia nueva del juego el topo está escondido
@@ -64,7 +74,7 @@ test('-------- Component: Check if a mole pops', (assert) => {
 });
 
 // Testear que se esconde
-test('-------- Component: Check if a mole hides', (assert) => {
+test('-------- Component: Check if a mole can to hide', (assert) => {
     const message = 'A mole exists';
     const expected = false;
 
@@ -114,11 +124,12 @@ test('-------- Component: Check if we can add six points', (assert) => {
 });
 
 // Comprobar cuando el topo ha sido golpeado: se ha escondido
-test('-------- Component: Check if no mole exists if mole was hit', (assert) => {
+test('-------- Component: Check if no mole exists after mole was hit', (assert) => {
     const message = 'Mole must be hidden';
     const expected = false;
 
     const wackAMole = WackAMole();
+    wackAMole.popMole();
     wackAMole.hit();
 
     const actual = wackAMole.isMoleVisible();
@@ -128,11 +139,28 @@ test('-------- Component: Check if no mole exists if mole was hit', (assert) => 
     assert.end();
 });
 
+// NOTA: utilizar espías para no usar en un test métodos que no deberían ser públicos (por ej, el isMoleVisible())
 
 // Comprobar cuando el topo ha sido golpeado: ha ganado un punto
-test('-------- Component: Check if we have 1 point after mole clicked', (assert) => {
+test('-------- Component: Check if we have 1 point after mole was clicked', (assert) => {
     const message = 'The score must be 1';
     const expected = 1;
+
+    const wackAMole = WackAMole();
+    wackAMole.popMole();
+    wackAMole.hit();
+
+    const actual = wackAMole.getScore();
+
+    assert.equal(actual, expected, message);
+
+    assert.end();
+});
+
+// Testear que no se añade punto si no existe topo al que golpear
+test('-------- Component: Check if we have no points added after we clicked somewhere but no mole is available', (assert) => {
+    const message = 'The score must be 0';
+    const expected = 0;
 
     const wackAMole = WackAMole();
     wackAMole.hit();
@@ -144,4 +172,69 @@ test('-------- Component: Check if we have 1 point after mole clicked', (assert)
     assert.end();
 });
 
-// NOTA: utilizar espías para no usar en un test métodos que no deberían ser públicos (por ej, el isMoleVisible())
+
+// Testear que no se añade punto si el topo ya se ha escondido
+test('-------- Component: Check if we have no points added after we hit a hole with no mole available', (assert) => {
+    const message = 'The score must be 0';
+    const expected = 0;
+
+    const wackAMole = WackAMole();
+    wackAMole.popMole();
+    wackAMole.hideMole();
+    wackAMole.hit();
+
+    const actual = wackAMole.getScore();
+
+    assert.equal(actual, expected, message);
+
+    assert.end();
+});
+
+// Testear que tenemos 6 agujeros donde colocar un topo
+test('-------- Component: Check if we have 6 holes where put a mole', (assert) => {
+    const message = 'The need 6 holes';
+    const expected = 6;
+
+    const wackAMole = WackAMole();
+
+    const actual = wackAMole.holes.length;
+
+    assert.equal(actual, expected, message);
+
+    assert.end();
+});
+
+// Testear que el topo puede salir al azar en cualquier agujero
+test('-------- Component: Check if we can put a mole in a random hole', (assert) => {
+    const message = 'The mole is not in that hole';
+    const expected = true;
+
+    const wackAMole = WackAMole();
+
+    // si hacemos el random dentro de popMole, no hay forma de comprobar que efectivamente ha sido random
+    // por tanto, le pasamos de forma random dónde queremos el mole
+    const max = 5, min = 0;
+    const random = Math.floor(Math.random() * (max - min)) + min;
+    wackAMole.popMole(random);
+
+    const actual = wackAMole.isMoleVisible(random);
+
+    assert.equal(actual, expected, message);
+
+    assert.end();
+});
+
+// El topo no puede salir en la última posición usada
+test('-------- Component: Check if mole will never pop in the same hole last mole used', (assert) => {
+    const message = 'Mole used last hole used';
+    const expected = false;
+
+    const wackAMole = WackAMole();
+
+    wackAMole.popMole(3);
+    const actual = wackAMole.popMole(3);
+
+    assert.equal(actual, expected, message);
+
+    assert.end();
+});
